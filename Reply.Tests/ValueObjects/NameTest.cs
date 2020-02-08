d3027d7a90b5b508b1e9a2afd84b3a40ec3f3e23
Edvaldo Farias <edvaldofarias.santana@gmail.com>
2020-02-07 23:59:00 -0300

    Correção do NameTest

--- Reply.Tests/ValueObjects/NameTest.cs
+++ Reply.Tests/ValueObjects/NameTest.cs
@@ -10,25 +10,29 @@ namespace Reply.Tests.ValueObjects
     [TestClass]
     public class NameTest
     {
+        //Possibilidade de vários teste usando DataRow
         [TestMethod]
-        public  void ShouldReturnFourNotificationWhenNameIsNotValid()
+        [DataRow("", "123456789012345678901234567890123456789012345678901")]
+        [DataRow("123456789012345678901234567890123456789012345678901", "")]
+        [DataRow("", "")]
+        [DataRow("123456789012345678901234567890123456789012345678901", "123456789012345678901234567890123456789012345678901")]
+        [DataRow("Ronaldo", "")]
+        [DataRow("", "Fenomeno")]
+        public  void ShouldReturnFourNotificationWhenNameIsNotValid(string firstName, string lastName)
         {
-            var name = new Name("", "123456789012345678901234567890123456789012345678901");
-            var name1 = new Name("123456789012345678901234567890123456789012345678901", "");
-
-            name.AddNotifications(name1);
-            
+            var name = new Name(firstName, lastName);
+            // Não se utilizar mais de um Assert em teste, e um má prática, caso o objeto seja inválido
+            // então está certo o teste, só testa todos os cenários
             Assert.AreEqual(true, name.Invalid);
-            Assert.AreEqual(4, name.Notifications.Count);
         }
 
         [TestMethod]
-        public void ShouldReturnnWithoutNotificationWhenNameIsValid()
+        [DataRow("Matheus", "Rodrigues")]
+        //Teste com o limite de carácter e o mínimo de carácter possível
+        public void ShouldReturnnWithoutNotificationWhenNameIsValid(string firstName, string lastName)
         {
-            var name = new Name("Matheus", "Rodrigues");
-
+            var name = new Name(firstName, lastName);
             Assert.AreEqual(true, name.Valid);
-            Assert.AreEqual(0, name.Notifications.Count);
         }
     }
 }
